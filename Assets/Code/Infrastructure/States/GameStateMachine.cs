d61064300d5eafd548c32f81cbe2f38fe89f3d7c
Viktor <viktor.yakovenko.228@gmail.com>
2024-06-14 18:11:58 +0300

    Implemented Game State Machine

---
+++ Assets/Code/Infrastructure/States/GameStateMachine.cs
@@ -0,0 +1,46 @@
+using Code.Infrastructure.Services;
+using Code.Logic;
+using System;
+using System.Collections.Generic;
+
+namespace Code.Infrastructure.States
+{
+    public class GameStateMachine
+    {
+        private readonly Dictionary<Type, IExitableState> _states;
+        private IExitableState _currentState;
+
+        public GameStateMachine(SceneLoader sceneLoader, LoadingCurtain curtain, AllServices services)
+        {
+            _states = new Dictionary<Type, IExitableState>
+            {
+                //Add states
+            };
+        }
+
+        public void Enter<TState>() where TState : class, IState
+        {
+            IState state = ChangeState<TState>();
+            state.Enter();
+        }
+
+        public void Enter<TState, TPayload>(TPayload payload) where TState : class, IPayloadedState<TPayload>
+        {
+            TState state = ChangeState<TState>();
+            state.Enter(payload);
+        }
+
+        private TState ChangeState<TState>() where TState : class, IExitableState
+        {
+            _currentState?.Exit();
+
+            TState state = GetState<TState>();
+            _currentState = state;
+
+            return state;
+        }
+
+        private TState GetState<TState>() where TState : class, IExitableState =>
+            _states[typeof(TState)] as TState;
+    }
+}
---
+++ Assets/Code/Infrastructure/States/IState.cs
@@ -0,0 +1,17 @@
+﻿namespace Code.Infrastructure.States
+{
+    public interface IState : IExitableState
+    {
+        void Enter();
+    }
+
+    public interface IExitableState
+    {
+        void Exit();
+    }
+
+    public interface IPayloadedState<TPayload> : IExitableState
+    {
+        void Enter(TPayload payload);
+    }
+}
