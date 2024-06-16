5c8a6ead3b6d250961009ab15a2b1b1c63c007dd
Viktor <viktor.yakovenko.228@gmail.com>
2024-06-17 00:54:36 +0300

    Implemented Bootstrap state
    In future there will be registred services

---
+++ Assets/Code/Infrastructure/States/BootstrapState.cs
@@ -0,0 +1,37 @@
+﻿using Code.Infrastructure.Services;
+
+namespace Code.Infrastructure.States
+{
+    public class BootstrapState : IState
+    {
+        private readonly GameStateMachine _stateMachine;
+        private readonly SceneLoader _sceneLoader;
+        private readonly AllServices _services;
+
+        public BootstrapState(GameStateMachine stateMachine, SceneLoader sceneLoader, AllServices services)
+        {
+            _sceneLoader = sceneLoader;
+            _stateMachine = stateMachine;
+            _services = services;
+
+            RegisterServices();
+        }
+
+        public void Enter()
+        {
+            _sceneLoader.Load(Scenes.Initial, EnterLoadLevel);
+        }
+
+        public void Exit()
+        {
+
+        }
+
+        private void EnterLoadLevel() => _stateMachine.Enter<LoadProgressState>();
+
+        private void RegisterServices()
+        {
+            //TODO: Registration of services
+        }
+    }
+}
---
+++ Assets/Code/Scenes.cs
@@ -0,0 +1,7 @@
+﻿namespace Code
+{
+    public class Scenes
+    {
+        public const string Initial = "Initial";
+    }
+}
