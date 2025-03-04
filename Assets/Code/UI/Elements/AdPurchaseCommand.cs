f33454924ac99aceedb6c21e603f9f6e29255241
Viktor <viktor.yakovenko.228@gmail.com>
2025-03-04 23:03:31 +0200

    Added Ad Purchase Command

---
+++ Assets/Code/UI/Elements/AdPurchaseCommand.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.UI.Elements
+{
+    public class AdPurchaseCommand : CommandBase
+    {
+        public Image Image;
+        public Sprite ActivatedSprite;
+
+        [SerializeField] private bool _isBought;
+
+        public void Construct(bool isBought) =>
+            _isBought = isBought;
+
+        private void Awake()
+        {
+            if (_isBought)
+                UpdateSprite();
+        }
+
+        public override void Execute() =>
+            Buy();
+
+        private void Buy()
+        {
+            if (_isBought) return;
+
+            _isBought = !_isBought;
+            UpdateSprite();
+        }
+
+        private void UpdateSprite() =>
+            Image.sprite = ActivatedSprite;
+    }
+}
