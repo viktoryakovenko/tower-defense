43dc070c556a35166541907bbd155a177d4cd60c
Viktor <viktor.yakovenko.228@gmail.com>
2025-04-15 15:13:13 +0300

    Created Menu UI component for menu HUD

---
+++ Assets/Code/UI/Elements/MenuUI.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.UI.Elements
+{
+    public class MenuUI : MonoBehaviour
+    {
+        public List<OpenWindowCommand> OpenWindowCommands;
+    }
+}
