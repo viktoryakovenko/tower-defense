c1dad830d78705b4822864a6f0be6ed936006c48
Viktor <viktor.yakovenko.228@gmail.com>
2025-04-15 13:08:07 +0300

    Added Asset Provider

---
+++ Assets/Code/Infrastructure/AssetManagement/IAssets.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Code.Infrastructure.AssetManagement
+{
+    public interface IAssets
+    {
+        GameObject Instantiate(string path);
+        GameObject Instantiate(string path, Vector3 at);
+        GameObject Instantiate(string path, Transform parent);
+    }
+}
